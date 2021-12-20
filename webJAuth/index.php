d129b98df1e967c1a811558513c774e2ff99db65
rogerpb98 <rogerpb98@gmail.com>
2021-12-20 15:31:10 +0100

    stuff

---
+++ webJAuth/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+    $title = "Productos";
+    include 'templates/head.php';
+?>
+<body>
+    <header>
+        
+    </header>
+    <?php
+        include 'templates/nav.php';
+    ?>
+    <main>
+        <button>Check In</button>
+    </main>
+</body>
+</html>
