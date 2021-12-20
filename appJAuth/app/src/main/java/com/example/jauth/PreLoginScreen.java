65e18ae4d9c1e4afd23c5deee340020e6d620438
sergiocastillero <cf17sergio.castillero@iesjoandaustria.org>
2021-12-20 02:14:28 +0000

    Designing prelogin_screen.xml

--- appJAuth/app/src/main/java/com/example/jauth/PreLoginScreen.java
+++ appJAuth/app/src/main/java/com/example/jauth/PreLoginScreen.java
@@ -1,14 +1,14 @@
 package com.example.jauth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreLoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.prelogin_screen);
     }
 }
