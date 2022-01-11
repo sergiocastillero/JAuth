bc26b2ecf5bac3db6774a19b59cdba69239e6de9
rogerpb98 <rogerpb98@gmail.com>
2022-01-11 15:51:24 +0100

    init

---
+++ backendJAuth/provisional.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS jauth;
+CREATE DATABASE IF NOT EXISTS jauth;
+drop user if exists 'jauth'@'localhost';
+create user 'jauth'@'localhost' identified by '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'jauth'@'localhost';
+use jauth;
+
+DROP TABLE IF EXISTS Teachers;
+DROP TABLE IF EXISTS Fichar;
+
+CREATE TABLE Teachers(
+    id varchar(128) PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
+    email VARCHAR(25)
+);
+CREATE TABLE Fichar(
+    teacher_id varchar(128),
+    hora_ent TIMESTAMP NOT NULL,
+    hora_sal TIMESTAMP NOT NULL,
+    tipo ENUM('Entrada','Salida'),
+
+    FOREIGN KEY (teacher_id) REFERENCES Teachers(id)
+);
+
+INSERT INTO Teachers (id, name, last_name, pwd) VALUES ('mco7','Pepe','Villuela','pepitoloco123');
+INSERT INTO Teachers (id, name, last_name, pwd) VALUES ('meg4c9','Agustín','Hernandez','ahahahah');
+INSERT INTO Teachers (id, name, last_name, pwd) VALUES ('4wn36','Antonia','Benito','abenitoaaa');
+INSERT INTO Teachers (id, name, last_name, pwd) VALUES ('3f8j9','Maria','Del Carmen','mariadelc123');
