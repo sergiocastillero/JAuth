5e2991fa2e009a40b0daa5811a70e8b344142a4a
rogerpb98 <rogerpb98@gmail.com>
2022-01-14 17:04:13 +0100

    test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/user', function () {
-    return view('');
+
+
+
+Route::get('/prueba', function () {
+    return view('prueba');
 });
